d70d016294e3e68cc920261f792f2ac555247f3e
katlegorphele <katlegophele95@gmail.com>
2024-11-11 22:22:57 +0200

    modified .env

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,24 @@
 "use client";
+import dotenv from 'dotenv';
+dotenv.config();
 import { createThirdwebClient } from "thirdweb";
 import { scrollSepoliaTestnet, sepolia } from "thirdweb/chains";
 import { ThirdwebProvider, ConnectButton, useActiveAccount } from "thirdweb/react";
+import { use, useEffect, useState } from "react";
+
+const clientID = process.env.NEXT_PUBLIC_CLIENTID;
 
 
 
 const client = createThirdwebClient({
-  clientId: "CLIENT_ID"
+  clientId: clientID as string,
+
 })
 
 
 export default function Home() {
 
-
-
+  console.log(clientID);
   return (
     <ThirdwebProvider>
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
